6f592994c0cc82f8121bb94aa5417b4334f6c899
john aitken <johna321123@gmail.com>
2019-07-08 00:00:27 -0400

    oh god bootstrap why

--- src/components/mainpage.jsx
+++ src/components/mainpage.jsx
@@ -15,8 +15,38 @@ class MainPage extends Component{
                         height="150px"
                         width="500px" />
                 </div>
-                <div className="textbox">
-                    <button className="btn btn-dark">hey</button>
+                <div className="farg">
+                    <div className="container">
+                     <div className="row">
+                            <div className="gay col-sm-6">
+                              this is a column hahahsdahsdagshdgahsd
+                            </div>
+                            <div className="gay col-sm-6">
+                                this is another column askjdhaksjdhaksjhd
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="thing">
+                    <div className="container">
+                        <ul className="thumbnails">
+                            <li className="span4">
+                                <div className="thumbnail">
+                                    <h3>label</h3>
+                                    <p>so i was walking down the street the other day and this guy, this guy says to me hey what the fuck and i said hey man u gaybro and he said no bro so i walked away</p>
+                                    <button className="btn btn-dark">test</button>
+                                </div>
+                            </li>
+                
+                            <li className="span4">
+                                <div className="thumbnail">
+                                    <h3>label2</h3>
+                                    <p>so i was walking down the street the other day and this guy, this guy says to me hey what the fuc</p>
+                                    <button className="btn btn-dark">test</button>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         );
