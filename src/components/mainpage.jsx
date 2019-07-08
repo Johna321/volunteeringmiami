35edb2950b03d23ecb08ee87dda1333061d092cd
john aitken <johna321123@gmail.com>
2019-07-07 22:53:37 -0400

    fixed logo, added bootstrap

--- src/components/mainpage.jsx
+++ src/components/mainpage.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './volmiamlogo.svg';
+import logo from './volmiamilogo.png';
 import './mainpage.css';
 
 class MainPage extends Component{
     render(){
         return(
             <div className="parent">
-                <div className="main">
+                <div className="logo">
                     <img 
                         src={logo}
                         className="app-logo"
                         alt="volunteering miami logo"
                         height="150px"
                         width="500px" />
+                </div>
+                <div className="textbox">
+                    <button className="btn btn-dark">hey</button>
                 </div>
             </div>
         );
