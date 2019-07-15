5db304f42b1060fafbe491633754a15878b6dfcb
john aitken <johna321123@gmail.com>
2019-07-15 19:17:13 -0400

    routing plus other stuff

---
+++ src/components/pages/about/about.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './about.css';
+
+class About extends Component{
+    render(){
+        return(
+            <div className="about">
+                <div className="title">
+                    <h1>about us</h1>
+                </div>
+                <div className="paragraph">
+                    volunteering miami is a website.
+                </div>
+            </div>
+        );
+    }
+}
+
+export default About;
--- src/components/pages/mainpage/mainpage.jsx
+++ src/components/pages/mainpage/mainpage.jsx
@@ -1,35 +1,13 @@
 import React, { Component } from 'react';
-import logo from './volmiamilogo.png';
+
 import './mainpage.css';
 import bizpic from './bizimage.jpg';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
 class MainPage extends Component{
     render(){
         return(
             <div className="parent">
-             <div className="topbord">hahahaha comic sans</div>
-                <div className="main">
-                    <div className="top">
-                        <div className="row">
-                            <div className="col">
-                                <Router>
-                                    <Link to={'/about/'} />
-                                    <a href="/help" className="link">Help</a>
-                                    <a href="/registercomp" className="link">Register</a>
-                                </Router>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="logo">
-                        <img 
-                            src={logo}
-                            className="app-logo"
-                            alt="volunteering miami logo"
-                            height="150px"
-                            width="500px" />
-                    </div>
                     <div className="body">
                         <div className="container">
                             <div className="panel-group">
@@ -71,7 +49,7 @@ class MainPage extends Component{
                             </div>
                         </div>
                     </div>
-                </div>
+                
                 <div className="functionality">
                     <h6>inputs</h6>
                     <form onSubmit={this.handleSubmit}>
@@ -80,8 +58,7 @@ class MainPage extends Component{
                         company description
                         <input type="text" name="compdesc" onChange={this.compDesc} />
                         <input type="submit" value="submit" />
-                    </form>
-                    
+                    </form>  
                 </div>
             </div>
         );
