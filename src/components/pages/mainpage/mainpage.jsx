9ebea021370ec86c780ec260f5e53bd3789bb84a
john aitken <johna321123@gmail.com>
2019-12-01 22:48:48 -0500

    left indent, fixed button, couple other things

--- src/components/pages/mainpage/mainpage.jsx
+++ src/components/pages/mainpage/mainpage.jsx
@@ -8,41 +8,142 @@ import arrow from '../../../arrow.png'
 
 class MainPage extends Component{
     state = {
-        expanded: false,
-        rotation: 0
+        render: true,
+        1: {
+            expanded: false,
+            name:'Business Name 1',
+            hours:'20 hours',
+            summary:'Brief summary.',
+            location: '',
+            volunteersNeeded: 'We need volunteers!',
+            dailyServiceHours: '',
+            totalProjectServiceHours: '',
+            dayToDayHours: '',
+            credentials: '',
+            gpa: '',
+            certficiation: '',
+            talents: '',
+            languages: '',
+            dressCode: '',
+            anyFurtherSpecifications: ''
+        },
+        2: {
+            expanded: false,
+            name:'Business Name 2',
+            hours:'20 hours',
+            summary:'Brief summary.',
+            location: '',
+            volunteersNeeded: 'Please volunteer for us.',
+            dailyServiceHours: '',
+            totalProjectServiceHours: '',
+            dayToDayHours: '',
+            credentials: '',
+            gpa: '',
+            certficiation: '',
+            talents: '',
+            languages: '',
+            dressCode: '',
+            anyFurtherSpecifications: ''
+        },
+        3: {
+            expanded: false,
+            name:'Business Name 3',
+            hours:'20 hours',
+            summary:'Brief summary.',
+            location: '',
+            volunteersNeeded: '',
+            dailyServiceHours: '',
+            totalProjectServiceHours: '',
+            dayToDayHours: '',
+            credentials: '',
+            gpa: '',
+            certficiation: '',
+            talents: '',
+            languages: '',
+            dressCode: '',
+            anyFurtherSpecifications: ''
+        },
+        4: {
+            expanded: false,
+            name:'Business Name 4',
+            hours:'20 hours',
+            summary:'Brief summary.',
+            location: '',
+            volunteersNeeded: '',
+            dailyServiceHours: '',
+            totalProjectServiceHours: '',
+            dayToDayHours: '',
+            credentials: '',
+            gpa: '',
+            certficiation: '',
+            talents: '',
+            languages: '',
+            dressCode: '',
+            anyFurtherSpecifications: ''
+        },
+        5: {
+            expanded: false,
+            name:'Business Name 5',
+            hours:'20 hours',
+            summary:'Brief summary.',
+            location: '',
+            volunteersNeeded: '',
+            dailyServiceHours: '',
+            totalProjectServiceHours: '',
+            dayToDayHours: '',
+            credentials: '',
+            gpa: '',
+            certficiation: '',
+            talents: '',
+            languages: '',
+            dressCode: '',
+            anyFurtherSpecifications: ''
+        },
     }
     
-    applyButton = () =>{
-        if (this.state.expanded === false){
-            this.setState({expanded: true});
-            this.setState({rotation: 180})
+    applyButton = (id) =>{
+        if (this.state[id].expanded === false){
+            this.state[id].expanded = true;
+            this.setState({render: true});
         }
         else{
-            this.setState({expanded: false});
-            this.setState({rotation: 0});
+            this.state[id].expanded = false;
+            this.setState({render: true});
         }
     }
-    expandedInfo(){
-        if (this.state.expanded === false){
-            return <p></p>;
-        }
-        else{
-            return(<div className="moreInfo">
-            <h5>Location:</h5>
-            <h5>Volunteers Needed:</h5>
-            <h5>Daily Service Hours:</h5>
-            <h5>Total Project Service Hours:</h5>
-            <h5>Day-to-Day Hours:</h5>
-            <h5>Credentials:</h5>
-            <h6>GPA:</h6>
-            <h6>Certification</h6>
-            <h6>Talents:</h6>
-            <h6>Languages:</h6>
-            <h5>Dress Code:</h5>
-            <h5>Any Further Specifications:</h5>
-
-        </div>);
-        }
+    expandedInfo(id){
+        return (
+            <div>
+            <div className="panel panel-default">
+            <img src={bizpic} className="rounded float-right .img-thumbnail bizpic"alt="pic of business"/>
+        <h1>{this.state[id].name}</h1>
+            <div className="panel-body">
+        <h5>{this.state[id].hours}</h5>
+                {this.state[id].summary}
+                <div className="arrowdropdown">
+                    <input type="image" src={arrow} width="24" height="24" onClick={() => { this.applyButton(id) }} />
+                </div>
+            </div>
+            </div>
+            {this.state[id].expanded !== false ? (
+            <div className="panel panel-default" style={{marginLeft:100, marginTop: -80, paddingTop:0, zIndex: 0, borderTopLeftRadius: 0, borderTopRightRadius: 0, width:'86.5%'}}>
+            <div className="moreInfo" >
+            <h5>Location: {this.state[id].location}</h5>
+            <h5>Volunteers Needed: {this.state[id].volunteersNeeded}</h5>
+            <h5>Daily Service Hours: {this.state[id].dailyServiceHours}</h5>
+            <h5>Total Project Service Hours: {this.state[id].totalProjectServiceHours}</h5>
+            <h5>Day-to-Day Hours: {this.state[id].dayToDayHours}</h5>
+            <h5>Credentials: {this.state[id].credentials}</h5>
+            <h6>GPA: {this.state[id].gpa}</h6>
+            <h6>Certification: {this.state[id].certficiation}</h6>
+            <h6>Talents: {this.state[id].talents}</h6>
+            <h6>Languages: {this.state[id].languages}</h6>
+            <h5>Dress Code: {this.state[id].dressCode}</h5>
+            <h5>Any Further Specifications: {this.state[id].anyFurtherSpecifications}</h5>
+            </div>
+            </div>) : (<p></p>)}
+            </div>
+        );
     }
     render(){
         return(
@@ -58,66 +159,11 @@ class MainPage extends Component{
                     <div className="body">
                         <div className="container">
                             <div className="panel-group">
-                                
-                                <div className="panel panel-default">
-                                    <img src={bizpic} className="rounded float-right .img-thumbnail bizpic"alt="pic of business"/>
-                                    <h1>random business 1</h1>
-                                    
-                                    <div className="panel-body">
-                                        <h5>however many hours</h5>
-                                        (brief summary)
-                                        <div className="arrowdropdown">
-                                            <input type="image" src={arrow} width="24" height="24" onClick={ this.applyButton } />
-                                        </div>
-                                        <span>{ this.expandedInfo() }</span>
-                                    </div>
-                                </div>
-                                <div className="panel panel-default">
-                                    <img src={bizpic} className="rounded float-right .img-thumbnail bizpic"alt="pic of business"/> 
-                                    <h1>random business 2</h1>
-
-                                    <div className="panel-body">
-                                        (brief summary)
-                                        <div className="arrowdropdown">
-                                            <input type="image" src={arrow} width="24" height="24" onClick={ this.applyButton } />
-                                        </div>
-                                        <span>{ this.expandedInfo() }</span>
-                                    </div>
-                                    
-                                </div>
-                                <div className="panel panel-default">
-                                    <img src={bizpic} className="rounded float-right .img-thumbnail bizpic"alt="pic of business" />
-                                    <h1>random business 3</h1>
-                                    <div className="panel-body">
-                                        (brief summary)
-                                        <div className="arrowdropdown">
-                                            <input type="image" src={arrow} width="24" height="24" onClick={ this.applyButton } />
-                                        </div>
-                                        <span>{ this.expandedInfo() }</span>
-                                    </div>
-                                </div>
-                                <div className="panel panel-default">
-                                    <img src={bizpic} className="rounded float-right .img-thumbnail bizpic"alt="pic of business" />
-                                    <h1>random business 4</h1>
-                                    <div className="panel-body">
-                                        (brief summary)
-                                        <div className="arrowdropdown">
-                                            <input type="image" src={arrow} width="24" height="24" onClick={ this.applyButton } />
-                                        </div>
-                                        <span>{ this.expandedInfo() }</span>
-                                    </div>
-                                </div>
-                                <div className="panel panel-default">
-                                    <img src={bizpic} className="rounded float-right .img-thumbnail bizpic"alt="pic of business" />
-                                    <h1>random business 5</h1>
-                                    <div className="panel-body">
-                                        (brief summary)
-                                        <div className="arrowdropdown">
-                                            <input type="image" src={arrow} width="24" height="24" onClick={ this.applyButton } />
-                                        </div>
-                                        <span>{ this.expandedInfo() }</span>
-                                    </div>
-                                </div>
+                                { this.expandedInfo('1') }
+                                { this.expandedInfo('2') }
+                                { this.expandedInfo('3') }
+                                { this.expandedInfo('4') }
+                                { this.expandedInfo('5') }
                             </div>
                         </div>
                     </div>
